1a14e8fae6f2dc9e2fe03c2a5b5792b2f4bcd443
linoon <linoonlab7@hmail.com>
2022-12-13 17:27:18 +0700

    addlab7.3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
+int adiff(int A,int B){
+  int C;
+  C = A-B;
+  C = abs(C);
+  if (C%360<=180){
+    return C%360;
+  } else {
+    return 360-(C%360);
+  }
+}
 
 int main(){
   cout << adiff(180,270);
