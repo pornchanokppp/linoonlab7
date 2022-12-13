c427c239d635ce7ab9ab706fea46e3b6a3c1a318
linoon <linoonlab7@hmail.com>
2022-12-13 16:39:56 +0700

    addlab7_2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,27 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i= 0 , P[5000], k, result;
 	while(i < 5000){
-		P[i] = 2*i+1;
+		P[i] = 2*i+1 ;
 		i++;
 	}
+	cout << "Please input k: " ;
+	cin >> k ;
+	if(k>0){
+		i = 0 ;
+		result = 0 ;
+		while(i < 5000){
+			if(P[i]%k == 0){
+				result = result - P[i] ;
+			} else {
+				result = result + P[i] ;
+			}
+			i = i+1 ;
+		}
+		cout << "Result = " << result ;
+	} else 
+	cout << "Invalid input!!!" ;
 	
 	return 0;
 }
